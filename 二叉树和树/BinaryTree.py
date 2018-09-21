698d7fb390aed002689565969ec944ec5ef2d428
wenzhe <434330345@qq.com>
2018-09-22 01:19:51 +0800

    20180919-3

--- 二叉树和树/BinaryTree.py
+++ 二叉树和树/BinaryTree.py
@@ -11,11 +11,30 @@ class BinaryTree:
         self.root = None
     def is_empty(self):
         return self.root is None
+    def find_none_node(self,node,way=left):
+        #way的值left或right
+        self.way = way
+        if self.way == 'left':
+            self.p = node.left
+            if self.p.left is None:
+                return self.p.left
+        elif self.way == 'right':
+            self.p = node.right
+            if self.p.right is None:
+                return self.p.right
+        else:
+            raise  TypeError
+        self.find_none_node(self.p,self.way)
+
     def insertLeft(self,newNode):
         if self.root is not None:
             self.root = BinTNode(newNode)
-        elif self.root.left is None:
-            self.root.left = BinTNode(newNode)
         else:
-            t = BinaryTree(newNode)
-            t.left = self.l
+            self.N = self.find_none_node(self.root,way = left)
+            self.N.left = BinTNode(newNode)
+    def insertright(self,newNode):
+        if self.root is not None:
+            self.root = BinTNode(newNode)
+        else:
+            self.N = self.find_none_node(self.root,way = right)
+            self.N.right = BinTNode(newNode)
